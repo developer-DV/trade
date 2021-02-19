90e5204de0b5bcacc565610a1fca6f85434c6b8b
developer-DV <64646648+developer-DV@users.noreply.github.com>
2021-02-19 12:37:10 +0200

    Header, section main, section start, section review, section questions

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
-$(function(){
+$(function () {
+
+    $('.review__slider').slick({
+        infinite: false,
+        prevArrow: '<button type="button" class="slider-btn slider-btn__left"><img src="/images/arrow-prev.svg" alt="arrow-prev"></button>',
+        nextArrow: '<button type="button" class="slider-btn slider-btn__right"><img src="/images/arrow-next.svg" alt="arrow-next"></button>',
+        adaptiveHeight: true,
+    })
+
+
+    $('.questions__item-title').on('click', function () {
+        if ($(this).parent().hasClass('questions__item--active')) {
+            $(this).parent().removeClass('questions__item--active')
+        }
+        else {
+            $('.questions__item').removeClass('questions__item--active')
+            $(this).parent().addClass('questions__item--active')
+        }
+    })
 
 });
+
+

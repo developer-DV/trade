c287f0e4161a8a24014ad2a5f9631583a69d4d60
developer-DV <64646648+developer-DV@users.noreply.github.com>
2021-02-22 13:12:17 +0200

    Сhanging the path of images in the reviews section

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ $(function () {
 
     $('.review__slider').slick({
         infinite: false,
-        prevArrow: '<button type="button" class="slider-btn slider-btn__left"><img src="/images/arrow-prev.svg" alt="arrow-prev"></button>',
-        nextArrow: '<button type="button" class="slider-btn slider-btn__right"><img src="/images/arrow-next.svg" alt="arrow-next"></button>',
+        prevArrow: '<button type="button" class="slider-btn slider-btn__left"><img src="./images/arrow-prev.svg" alt="arrow-prev"></button>',
+        nextArrow: '<button type="button" class="slider-btn slider-btn__right"><img src="./images/arrow-next.svg" alt="arrow-next"></button>',
         adaptiveHeight: true,
     })
 
